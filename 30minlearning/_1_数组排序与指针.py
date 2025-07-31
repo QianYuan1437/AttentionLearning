a277e9ad600e0f577ba03dc816f14cabe0516a28
风陵晚 <92597256+USTB-3rd-zckj-techdepartment@users.noreply.github.com>
2025-07-31 22:24:48 +0800

    Update _1_数组排序与指针.py

--- 30minlearning/_1_数组排序与指针.py
+++ 30minlearning/_1_数组排序与指针.py
@@ -23,7 +23,7 @@
 # #######################################
 
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1, m, nums2, n):
         """
         Do not return anything, modify nums1 in-place instead.
         """
